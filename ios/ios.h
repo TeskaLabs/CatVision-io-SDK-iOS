2731f1a7ac7bdcd692ebdb27213a6ca35c648b6f
Ales Teska <ales.teska@teskalabs.com>
2017-09-28 00:31:19 +0200

    Add an iOS CatVision.io SDK project.

---
+++ ios/ios.h
@@ -0,0 +1,19 @@
+//
+//  ios.h
+//  ios
+//
+//  Created by Ales Teska on 28.9.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios.
+FOUNDATION_EXPORT double iosVersionNumber;
+
+//! Project version string for ios.
+FOUNDATION_EXPORT const unsigned char iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios/PublicHeader.h>
+
+
