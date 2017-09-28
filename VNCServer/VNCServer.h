5a3da71c36f3a5576ec001fc79ec10dbd3157c4e
Ales Teska <ales.teska@teskalabs.com>
2017-09-28 20:41:44 +0200

    VNC server is now a proper XCode framework.

---
+++ VNCServer/VNCServer.h
@@ -0,0 +1,19 @@
+//
+//  VNCServer.h
+//  VNCServer
+//
+//  Created by Ales Teska on 28.9.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VNCServer.
+FOUNDATION_EXPORT double VNCServerVersionNumber;
+
+//! Project version string for VNCServer.
+FOUNDATION_EXPORT const unsigned char VNCServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VNCServer/PublicHeader.h>
+
+
