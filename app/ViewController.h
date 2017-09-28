d27049393c802c0bce69aa7a79ef920078ae7a62
Ales Teska <ales.teska@teskalabs.com>
2017-09-28 18:09:15 +0200

    Add (empty) demo/test application.

---
+++ app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  app
+//
+//  Created by Ales Teska on 28.9.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app
+//
+//  Created by Ales Teska on 28.9.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
