f474790192c1b3d03b5059c3659b5600691c4980
Ales Teska <ales.teska@teskalabs.com>
2017-10-01 15:37:27 +0200

    Add a CatVision.io plugin for SeaCat skeleton.

---
+++ ios/CVIOSeaCatPlugin.h
@@ -0,0 +1,13 @@
+//
+//  CVIOSeaCatPlugin.h
+//  CatVisionIO
+//
+//  Created by Ales Teska on 1.10.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#ifndef CVIOSeaCatPlugin_h
+#define CVIOSeaCatPlugin_h
+
+
+#endif /* CVIOSeaCatPlugin_h */
---
+++ ios/CVIOSeaCatPlugin.m
@@ -0,0 +1,9 @@
+//
+//  CVIOSeaCatPlugin.m
+//  ios
+//
+//  Created by Ales Teska on 1.10.17.
+//  Copyright © 2017 TeskaLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
